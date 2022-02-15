214f76a55d5b4aefa936a82d88623cf77aca02a3
alensaito1 <erwinsmithscout1@gmail.com>
2022-02-15 16:45:02 +0530

    refactor(): encapsulate request methods

--- lib/utils/request.dart
+++ lib/utils/request.dart
@@ -1,19 +1,32 @@
 import 'package:http/http.dart' as http;
 
-Future<String> get(String url) async {
-  final response = await http.get(Uri.parse(url));
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw Exception('An Error Occurred');
+class Request {
+  static Future<String> get(
+    String url, {
+    Map<String, String>? headers,
+  }) async {
+    final response = await http.get(Uri.parse(url), headers: headers);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('An Error Occurred');
+    }
   }
-}
 
-Future post(String url, Map<String, dynamic> body) async {
-  final response = await http.post(Uri.parse(url), body: body);
-  if (response.statusCode == 200) {
-    return response.body;
-  } else {
-    throw Exception('An Error Occurred');
+  static Future post(
+    String url,
+    Map<String, dynamic> body, {
+    Map<String, String>? headers,
+  }) async {
+    final response = await http.post(
+      Uri.parse(url),
+      body: body,
+      headers: headers,
+    );
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('An Error Occurred');
+    }
   }
 }
