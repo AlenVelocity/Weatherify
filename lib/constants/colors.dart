31208751e20400635722609d3a7c8472d896abb1
alensaito1 <erwinsmithscout1@gmail.com>
2022-02-15 09:01:09 +0530

    feat(): add color constants

---
+++ lib/constants/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WeatherColors {
+  static const morning = Color(0xFFF78A55);
+  static const day = Color(0xFFB56291);
+  static const night = Color(0xFF5E5E5E);
+}
+
+class Grey {
+  static const regular = Color(0xFF585D60);
+  static const translucent = Color(0x22585D60);
+}
