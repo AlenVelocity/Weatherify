1d737c67c6e945701d54aa52d6f4d7589336dbec
alensaito1 <erwinsmithscout1@gmail.com>
2022-02-15 09:01:22 +0530

    feat(): add text style constants

---
+++ lib/constants/text_style.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:weatherify/constants/colors.dart';
+
+class WeatherTextStyle {
+  static const TextStyle title = TextStyle(
+    fontFamily: 'Khula',
+    fontSize: 60.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle titleB = TextStyle(
+    fontFamily: 'Khula',
+    fontSize: 60.0,
+    color: Colors.black,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle title2 = TextStyle(
+    fontFamily: 'Khula',
+    fontSize: 25.0,
+    color: Color(0xFF585D60),
+    fontWeight: FontWeight.w500,
+  );
+
+  static const TextStyle subTitle = TextStyle(
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w700,
+    fontSize: 20.0,
+    color: Colors.black,
+  );
+
+  static const TextStyle cardHour = TextStyle(
+    fontFamily: 'Khula',
+    fontSize: 15.0,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle cardTemp = TextStyle(
+    fontFamily: 'Khula',
+    fontSize: 25.0,
+    color: Colors.white,
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle mediumRegularBlack = TextStyle(
+    color: Colors.black,
+    fontSize: 23.0,
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle mediumRegularGrey = TextStyle(
+    color: Grey.translucent,
+    fontSize: 23.0,
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle regularBlack = TextStyle(
+    color: Colors.black,
+    fontSize: 17.0,
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle regularGrey = TextStyle(
+    color: Grey.translucent,
+    fontSize: 17.0,
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w600,
+  );
+
+  static const TextStyle smallGrey = TextStyle(
+    color: Grey.translucent,
+    fontSize: 12.0,
+    fontFamily: 'Khula',
+    fontWeight: FontWeight.w600,
+  );
+}
